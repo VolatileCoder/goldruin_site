b8084a3a52d3da8826a9c75341d6c8282cae0281
VolatileCoder <mark.milley@binaryswitch.com>
2026-08-31 07:58:36 -0400

    Another Spectating Fix Attempt

--- docs/play/goldruin7.js
+++ docs/play/goldruin7.js
@@ -1,4 +1,4 @@
-const VERSION = "v7.26.8.31.20 BETA"
+const VERSION = "v7.26.8.31.24 BETA"
 class Controller{
     up = 0;
     left = 0;
@@ -8342,6 +8342,7 @@ class Adventurer extends Character{
         if(this.state==State.DEAD || this.tag != "Server"){
             if(game.level){
                 this.room = game.level.rooms[0];
+                this.state = State.DEAD;
             }
         }
         this.clear();
